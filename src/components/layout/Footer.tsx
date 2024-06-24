90756b90b967a81c415babf6371146231b7de71c
Harry Dhillon <hdhillon478@gmail.com>
2024-06-24 11:17:23 +0100

    WIP: Begin building out the core layout components

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <small>
+        <p> Intended solely for educational purposes</p>
+      </small>
+
+      <p>
+        <span className='u-bold'>0</span> total jobs available
+      </p>
+    </footer>
+  );
+}
