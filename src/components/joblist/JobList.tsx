a34295ce34507a241431318a31cc90625eb6ad6b
Harry Dhillon <hdhillon478@gmail.com>
2024-06-26 21:03:42 +0100

    Update joblist to render job items

--- src/components/joblist/JobList.tsx
+++ src/components/joblist/JobList.tsx
@@ -1,3 +1,17 @@
-export default function JobList() {
-  return <ul className='job-list'></ul>;
+import JobListItem from "./JobListItem";
+
+import { TJobItem } from "../../lib/types";
+
+type JobListProps = {
+  jobItems: TJobItem[];
+};
+
+export default function JobList({ jobItems }: JobListProps) {
+  return (
+    <ul className='job-list'>
+      {jobItems.map((jobItem) => (
+        <JobListItem key={jobItem.id} jobItem={jobItem} />
+      ))}
+    </ul>
+  );
 }
