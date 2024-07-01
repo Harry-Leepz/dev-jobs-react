adfce24d9afe53b3c020d072ca59e18bc0c0144f
Harry Dhillon <hdhillon478@gmail.com>
2024-07-01 21:05:21 +0100

    Add custom hook to get active id for a job item from url hash

---
+++ src/hooks/useActiveId.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+/*
+  The useActiveId hook is responsible for getting the active ID from the URL hash.
+  It returns the active ID.
+*/
+
+export default function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = window.location.hash.slice(1);
+      setActiveId(id ? parseInt(id) : null);
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
