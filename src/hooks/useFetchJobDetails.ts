2d129fd52ca561834b0ce7cb9fb04ca710bf569f
Harry Dhillon <hdhillon478@gmail.com>
2024-07-03 21:37:15 +0100

    Refactor: useFetchJobDetails custom hook with react-query

--- src/hooks/useFetchJobDetails.ts
+++ src/hooks/useFetchJobDetails.ts
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { BASE_API_URL } from "../lib/constants";
-import { TJobDetails } from "../lib/types";
 
 /*
   The useFetchJobDetails hook is responsible for fetching job details from the API.
   It takes an id parameter and returns jobDetails and loading.
 */
 
+const fetchJobDetails = async (id: number) => {
+  if (!id) return;
+  const response = await fetch(`${BASE_API_URL}/${id}`);
+  const data = await response.json();
+  return data.jobItem;
+};
+
 export default function useFetchJobDetails(id: number | null) {
-  const [jobDetails, setJobDetails] = useState<TJobDetails | null >(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-    const fetchJobDetails = async () => {
-      setIsLoading(true);
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      setIsLoading(false);
-      setJobDetails(data.jobItem);
-    };
-    fetchJobDetails();
-  }, [id]);
-
-  return [jobDetails, isLoading] as const;
+  const { data, isLoading } = useQuery(
+    ["jobDetails", id],
+    () => (id ? fetchJobDetails(id) : null),
+    {
+      staleTime: 1000 * 60 * 60, // 1 hour
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id), // only fetch when id is truthy
+      onError: () => {},
+    }
+  );
+  return { jobDetails: data, isLoading };
 }
