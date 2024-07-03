6f2b21b176acab95033e2d2e2e8f6c7f54926285
Harry Dhillon <hdhillon478@gmail.com>
2024-07-03 21:57:56 +0100

    Add typing to job details fetch response

--- src/hooks/useFetchJobDetails.ts
+++ src/hooks/useFetchJobDetails.ts
@@ -1,21 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { BASE_API_URL } from "../lib/constants";
+import { TJobDetails } from "../lib/types";
+
+type JobDetailsApiresponse = {
+  public: boolean;
+  jobItem: TJobDetails;
+};
 
 /*
   The useFetchJobDetails hook is responsible for fetching job details from the API.
   It takes an id parameter and returns jobDetails and loading.
 */
 
-const fetchJobDetails = async (id: number) => {
-  if (!id) return;
+const fetchJobDetails = async (id: number): Promise<JobDetailsApiresponse> => {
   const response = await fetch(`${BASE_API_URL}/${id}`);
   const data = await response.json();
-  return data.jobItem;
+  return data;
 };
 
 export default function useFetchJobDetails(id: number | null) {
-  const { data, isLoading } = useQuery(
+  const { data, isInitialLoading } = useQuery(
     ["jobDetails", id],
     () => (id ? fetchJobDetails(id) : null),
     {
@@ -26,5 +31,7 @@ export default function useFetchJobDetails(id: number | null) {
       onError: () => {},
     }
   );
-  return { jobDetails: data, isLoading };
+  const jobDetails = data?.jobItem;
+  const isLoading = isInitialLoading;
+  return { jobDetails, isLoading };
 }
