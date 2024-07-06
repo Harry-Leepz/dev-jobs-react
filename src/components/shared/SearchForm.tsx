aaca9ff7f975d521a90f498b105b310ed516bdce
Harry Dhillon <hdhillon478@gmail.com>
2024-07-06 13:05:04 +0100

    Add submit handler function into component

--- src/components/shared/SearchForm.tsx
+++ src/components/shared/SearchForm.tsx
@@ -1,14 +1,16 @@
 type SearchFormProps = {
   searchText: string;
-  onSubmitHandler: (e: React.FormEvent<HTMLFormElement>) => void;
   onChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
 export default function SearchForm({
   searchText,
-  onSubmitHandler,
   onChangeHandler,
 }: SearchFormProps) {
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <form onSubmit={onSubmitHandler} className='search'>
       <button>
