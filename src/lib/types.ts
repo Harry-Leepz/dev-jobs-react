f9d79a918102760827183397ebff300af9a9fef9
Harry Dhillon <hdhillon478@gmail.com>
2024-07-07 12:25:16 +0100

    Extract types for page direction and sorting

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,25 +1,29 @@
 export type TJobItem = {
-  id: number
-  title: string
-  badgeLetters: string
-  company: string
-  relevanceScore: number
-  daysAgo: number
+  id: number;
+  title: string;
+  badgeLetters: string;
+  company: string;
+  relevanceScore: number;
+  daysAgo: number;
 };
 
 export type TJobDetails = {
-  id: number
-  description: string
-  qualifications: string[]
-  reviews: string[]
-  title: string
-  badgeLetters: string
-  company: string
-  duration: string
-  salary: string
-  location: string
-  relevanceScore: number
-  daysAgo: number
-  coverImgURL: string
-  companyURL: string
+  id: number;
+  description: string;
+  qualifications: string[];
+  reviews: string[];
+  title: string;
+  badgeLetters: string;
+  company: string;
+  duration: string;
+  salary: string;
+  location: string;
+  relevanceScore: number;
+  daysAgo: number;
+  coverImgURL: string;
+  companyURL: string;
 };
+
+export type TSortingOption = "relevant" | "recent";
+
+export type TPageDirection = "next" | "previous";
