18ea85b654e5a25f40e7d50463648d40a9779be2
Harry Dhillon <hdhillon478@gmail.com>
2024-07-09 16:11:29 +0100

    Add logic to bookmark a jobitem

--- src/components/bookmarks/BookmarkIcon.tsx
+++ src/components/bookmarks/BookmarkIcon.tsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
+import { BookmarksContext } from "../../contexts/bookmarks/BookmarksContextProvider";
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 
-export default function BookmarkIcon() {
+type BookmarkIconProps = {
+  id: number;
+};
+
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { bookmarkedIds, toggleBookmarkHandler } = useContext(BookmarksContext);
+
+  const preventDefault = (e: React.MouseEvent) => {
+    toggleBookmarkHandler(id);
+    e.stopPropagation();
+    e.preventDefault();
+  };
   return (
-    <button className='bookmark-btn'>
-      <BookmarkFilledIcon />
+    <button onClick={preventDefault} className='bookmark-btn'>
+      <BookmarkFilledIcon
+        className={`${bookmarkedIds.includes(id) && "filled"}`}
+      />
     </button>
   );
 }
