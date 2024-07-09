2540b50295caf7f38d51527caa5406e31547cf61
Harry Dhillon <hdhillon478@gmail.com>
2024-07-09 16:10:53 +0100

    Add logic to store and get bookmarkedIds from local storage

--- src/contexts/bookmarks/BookmarksContextProvider.tsx
+++ src/contexts/bookmarks/BookmarksContextProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 type BookmarksContextProviderProps = {
   children: React.ReactNode;
@@ -14,8 +14,14 @@ export const BookmarksContext = createContext<BookmarksContext | null>(null);
 export default function BookmarksContextProvider({
   children,
 }: BookmarksContextProviderProps) {
-  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(
+    () => JSON.parse(localStorage.getItem("bookmarkedIds")!) || []
+  );
   console.log(bookmarkedIds);
+
+  useEffect(() => {
+    localStorage.setItem("bookmarkedIds", JSON.stringify(bookmarkedIds));
+  }, [bookmarkedIds]);
 
   const toggleBookmarkHandler = (id: number) => {
     if (bookmarkedIds.includes(id)) {
