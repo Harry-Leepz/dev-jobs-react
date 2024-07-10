d6b17d9323dab5b50e8153c99e14b9d269bc4620
Harry Dhillon <hdhillon478@gmail.com>
2024-07-10 08:45:07 +0100

    Update comments to include params used

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 /*
   The useDebounce hook is responsible for debouncing a value.
   It returns the debounced value.
+  Parameters:
+  value: The value to debounce.
+  delay: The delay in milliseconds.
 */
 
 export default function useDebounce<T>(value: T, delay = 1000): T {
