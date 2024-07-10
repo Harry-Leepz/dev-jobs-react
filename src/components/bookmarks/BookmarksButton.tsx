8d6988ffa07e72b4c833cfc7603556189a4e5a71
Harry Dhillon <hdhillon478@gmail.com>
2024-07-10 14:19:28 +0100

    Add logic to open or close the bookmark popover

--- src/components/bookmarks/BookmarksButton.tsx
+++ src/components/bookmarks/BookmarksButton.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import { TriangleDownIcon } from "@radix-ui/react-icons";
+import BookmarksPopover from "./BookmarksPopover";
 
 export default function BookmarksButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpenClickHandler = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <section>
-      <button className='bookmarks-btn'>
+      <button onClick={onOpenClickHandler} className='bookmarks-btn'>
         Bookmarks <TriangleDownIcon />
       </button>
+
+      {isOpen && <BookmarksPopover />}
     </section>
   );
 }
