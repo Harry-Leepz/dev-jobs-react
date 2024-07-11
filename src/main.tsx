7c05f4f7690dfbfec5c72268543744c46f3312b8
Harry Dhillon <hdhillon478@gmail.com>
2024-07-11 13:59:17 +0100

    Add activeid context provider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import BookmarksContextProvider from "./contexts/bookmarks/BookmarksContextProvider.tsx";
+import ActiveIdContextProvider from "./contexts/activeId/ActiveIdContextProvider.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BookmarksContextProvider>
-        <App />
-      </BookmarksContextProvider>
+      <ActiveIdContextProvider>
+        <BookmarksContextProvider>
+          <App />
+        </BookmarksContextProvider>
+      </ActiveIdContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
