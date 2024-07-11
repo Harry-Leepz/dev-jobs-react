ab264d3d40d3d16ee2985f632490281fef0582e1
Harry Dhillon <hdhillon478@gmail.com>
2024-07-11 16:36:06 +0100

    Add job data context provider wrapper to app

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BookmarksContextProvider from "./contexts/bookmarks/BookmarksContextProvider.tsx";
 import ActiveIdContextProvider from "./contexts/activeId/ActiveIdContextProvider.tsx";
 import SearchTextContextProvider from "./contexts/search/SearchTextContextProvider.tsx";
+import JobDataContextProvider from "./contexts/jobData/JobDataContextProvider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +18,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ActiveIdContextProvider>
         <BookmarksContextProvider>
           <SearchTextContextProvider>
-            <App />
+            <JobDataContextProvider>
+              <App />
+            </JobDataContextProvider>
           </SearchTextContextProvider>
         </BookmarksContextProvider>
       </ActiveIdContextProvider>
