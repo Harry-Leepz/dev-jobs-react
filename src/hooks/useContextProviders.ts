819b45a0559accc3e052a877bfe15f5d5036ab06
Harry Dhillon <hdhillon478@gmail.com>
2024-07-11 13:58:53 +0100

    Create a custom hooke to make use of active id context

--- src/hooks/useContextProviders.ts
+++ src/hooks/useContextProviders.ts
@@ -1,5 +1,7 @@
 import { useContext } from "react";
+
 import { BookmarksContext } from "../contexts/bookmarks/BookmarksContextProvider";
+import { ActiveIdContext } from "../contexts/activeId/ActiveIdContextProvider";
 
 /*
   The useBookmarksContext hook is responsible for getting the bookmarks context.
@@ -15,3 +17,18 @@ export function useBookmarksContext() {
   }
   return context;
 }
+
+/*
+  The useActiveIdContext hook is responsible for getting the activeId context.
+  It returns the activeId context.
+*/
+
+export const useActiveIdContext = () => {
+  const context = useContext(ActiveIdContext);
+  if (!context) {
+    throw new Error(
+      "useActiveId must be used within a ActiveIdContextProvider"
+    );
+  }
+  return context;
+};
