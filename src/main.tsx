57b5161d52b7e6465c36c9f67bf94903110969ff
Harry Dhillon <hdhillon478@gmail.com>
2024-07-11 16:18:54 +0100

    Add search text context provider wrapper to app

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import BookmarksContextProvider from "./contexts/bookmarks/BookmarksContextProvider.tsx";
 import ActiveIdContextProvider from "./contexts/activeId/ActiveIdContextProvider.tsx";
+import SearchTextContextProvider from "./contexts/search/SearchTextContextProvider.tsx";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ActiveIdContextProvider>
         <BookmarksContextProvider>
-          <App />
+          <SearchTextContextProvider>
+            <App />
+          </SearchTextContextProvider>
         </BookmarksContextProvider>
       </ActiveIdContextProvider>
     </QueryClientProvider>
